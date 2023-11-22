ed6ea25a059106ea70826edcef3e3d527f736c10
imLukePex <luca.pessione@gmail.com>
2023-11-22 18:35:05 +0100

    javascript structure mail checklist

--- js/script.js
+++ js/script.js
@@ -6,8 +6,9 @@ const subscribers = ["luca.pessione@gmail.com", "pincopallo@gmail.com", "frances
 console.log(`Email personale: ${subscribers[0]}`);
 
 // creo variabili di avvenuto o non avvenuto accesso
-let positiveAccess = "Bentornato Luca! <br> E' un piacere rivederti.";
-let negativeAccess = "Qualcosa è andato storto... <br> Non sei registrato nei nostri sistemi!";
+let logged = false;
+let positiveAccess = `Bentornato/a <br> E' un piacere rivederti.`;
+let negativeAccess = "Qualcosa è andato storto... <br> Non sei registrato/a nei nostri sistemi!";
 
 // Chiediamo all'utente la sua email
 const userEmail = prompt("Inserisci la tua email!");
@@ -15,13 +16,16 @@ const userEmail = prompt("Inserisci la tua email!");
 console.log("La tua email è:", userEmail);
 
 // Controlliamo che la mail inserita sia tra quelle che può accedere
-for (let i = 0; i <= subscribers.length; i++) {
+for (let i = 0; i < subscribers.length; i++) {
     console.log(subscribers[i]);
-    // Stampiamo un messaggio dell'esito del controllo
-    if (subscribers[i] === userEmail) {
-        document.getElementById("message").innerHTML = positiveAccess;
-    } else {
-        document.getElementById("message").innerHTML = negativeAccess;
+    if (userEmail === subscribers[i]) {
+        logged = true;
     }
 }
 
+// Stampiamo un messaggio dell'esito del controllo
+if (logged === true) {
+    document.getElementById("message").innerHTML = positiveAccess;
+} else {
+    document.getElementById("message").innerHTML = negativeAccess;
+}
